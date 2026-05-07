f7ba932b52458c4cadc1096349768f32df5722a1
OneX131X <kurniawanrasyidi1321@gmail.com>
2026-05-07 14:37:54 +0700

    navbar

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -45,10 +45,10 @@
 
       if (currentScroll > lastScroll && currentScroll > 500) {
         // scrolling down
-        navbar.classList.add('-translate-y-322');
+        navbar.classList.add('-translate-y-32');
       } else {
         // scrolling up
-        navbar.classList.remove('-translate-y-322');
+        navbar.classList.remove('-translate-y-32');
       }
 
       lastScroll = currentScroll;
@@ -57,7 +57,7 @@
     // Show when mouse goes near top
     document.addEventListener('mousemove', function(e) {
       if (e.clientY < 80) {
-        navbar.classList.remove('-translate-y-322');
+        navbar.classList.remove('-translate-y-32');
       }
     });
   </script>
